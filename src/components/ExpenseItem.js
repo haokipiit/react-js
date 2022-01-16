add5c18e82c1566a5bb6e9fb059c7ab0536b2568
thanglenmang <haokip.dev@gmail.com>
2022-01-16 22:14:59 +0530

    props re-commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         {/* A javascript can be added inside here using the curly brace */}
